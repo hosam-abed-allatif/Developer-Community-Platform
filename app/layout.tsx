1e5f79a3cc666c828dbaee1ffe931b8e07f0bdaa
Hosam AbedAlLatif <hosam.abedallatif1@gmail.com>
2026-07-28 01:03:51 +0300

    home created

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         >
         <body className="min-h-full flex flex-col">
         <Navbar/>
-        {children}
+        <div className={"p-10"}>{children}</div>
         <Footer/>
         </body>
         </html>
