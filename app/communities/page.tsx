eb8d01281c5b2d76cd01f821948a46f006a15b25
Hosam AbedAlLatif <hosam.abedallatif1@gmail.com>
2026-07-28 01:28:10 +0300

    communities page created

---
+++ app/communities/page.tsx
@@ -0,0 +1,12 @@
+import HeroSection from "@/components/HeroSection";
+
+const communities = () => {
+    return (
+        <HeroSection small={"communities"} h1={
+            <>Built Around <br/> What You Code.</>
+        }
+                     p={"Dive into topic-focused groups, stay updated on breaking trends, and collaborate directly with engineers who share your passion."}/>
+    )
+}
+
+export default communities;
