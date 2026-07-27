dd07cf16177e160490102fdb69ec9dd5d68d57df
Hosam AbedAlLatif <hosam.abedallatif1@gmail.com>
2026-07-28 01:34:19 +0300

    developers page created

---
+++ app/developers/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "@/components/HeroSection";
+
+const Developers = () => {
+    return (
+        <HeroSection small={"Developers"} h1={
+            <>Discover & Connect <br/> with World-Class Developers.</>
+        }
+                     p={"Find engineers building with your favorite tech stacks, share expertise, and expand your professional engineering circle."}/>
+    )
+}
+export default Developers;
