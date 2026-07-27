82bbc9dc4e972b8ec1a4f3cb2bc0511719ec8ee3
Hosam AbedAlLatif <hosam.abedallatif1@gmail.com>
2026-07-28 02:26:56 +0300

    adjusted [slug] page to view community added fake join button

--- app/communities/[slug]/page.tsx
+++ app/communities/[slug]/page.tsx
@@ -1,16 +1,23 @@
 import HeroSection from "@/components/HeroSection";
+import {COMMUNITIES} from "@/backend/communities"
 
 const Community = async ({params,}: {
     params: Promise<{ slug: string }>;
 }) => {
     const {slug} = await params;
+    const community = COMMUNITIES.find((community) => community.slug === slug);
 
     return (
         <HeroSection
-            small="communities/[slug]"
-            h1={slug}
-            p={`Welcome to the ${slug} community!`}
-        />
+            small={`${community.totalMembers} members`}
+            h1={community.title}
+            p={community.description}
+        >
+            <button
+                className={"bg-cyan-900 border rounded-full mt-10 px-3 py-2 cursor-pointer hover:bg-cyan-500 hover:text-cyan-900"}>Join
+                Community
+            </button>
+        </HeroSection>
     );
 };
 
--- app/communities/page.tsx
+++ app/communities/page.tsx
@@ -1,10 +1,9 @@
 import HeroSection from "@/components/HeroSection";
 import Link from "next/link";
-import {COMMUNITIES} from "../../backend/communities"
+import {COMMUNITIES} from "@/backend/communities"
 import CommunityCard from "@/components/CommunityCard";
 
 const Communities = () => {
-    const communities = COMMUNITIES;
     return (
         <>
             <HeroSection small={"communities"} h1={
@@ -21,7 +20,7 @@ const Communities = () => {
                       href={"/communities/ui-ux"}>ui-ux</Link>
             </div>
             <div className={"mx-72 mt-10 grid grid-cols-3 gap-3"}>
-                {communities.map((community) => (
+                {COMMUNITIES.map((community) => (
                     <CommunityCard key={community.slug} slug={community.slug} totalMembers={community.totalMembers}
                                    title={community.title} description={community.description}/>
                 ))}
