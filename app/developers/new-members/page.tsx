46ac2104eb23888ac27a1e792aaf2440ee09c6fa
Hosam AbedAlLatif <hosam.abedallatif1@gmail.com>
2026-07-28 02:51:58 +0300

    added developers to nested pages based on type

--- app/developers/new-members/page.tsx
+++ app/developers/new-members/page.tsx
@@ -1,11 +1,23 @@
 import HeroSection from "@/components/HeroSection";
+import {DEVELOPERS} from "@/backend/developers";
+import DeveloperCard from "@/components/DeveloperCard";
 
 const NewMembers = () => {
     return (
-        <HeroSection small={"New Members"} h1={
-            <>Welcome to the Next <br/> Generation of Creators.</>
-        }
-                     p={"Say hello to our newest community members! Discover developers who recently joined, explore their technical interests, and help them feel right at home."}/>
+      <>
+          <HeroSection small={"New Members"} h1={
+              <>Welcome to the Next <br/> Generation of Creators.</>
+          }
+                       p={"Say hello to our newest community members! Discover developers who recently joined, explore their technical interests, and help them feel right at home."}/>
+
+          <div className={"mx-72 mt-10 grid grid-cols-3 gap-3"}>
+              {DEVELOPERS.map((developer) => (
+                  developer.type === "new-members" &&
+                  <DeveloperCard key={developer.id} id={developer.id}
+                                 username={developer.username}
+                                 description={developer.description} pic={developer.pic}/>
+              ))}
+          </div></>
     )
 }
 export default NewMembers
--- app/developers/top-rated/page.tsx
+++ app/developers/top-rated/page.tsx
@@ -1,11 +1,23 @@
 import HeroSection from "@/components/HeroSection";
+import {DEVELOPERS} from "@/backend/developers";
+import DeveloperCard from "@/components/DeveloperCard";
 
 const TopRated = () => {
     return (
-        <HeroSection small={"Top Rated"} h1={
-            <>Celebrating the Best <br/> in the Community.</>
-        }
-                     p={"Meet our top-rated developers, top open-source contributors, and community leaders. Recognized for their exceptional code contributions, mentorship, and technical insights."}/>
+        <>
+            <HeroSection small={"Top Rated"} h1={
+                <>Celebrating the Best <br/> in the Community.</>
+            }
+                         p={"Meet our top-rated developers, top open-source contributors, and community leaders. Recognized for their exceptional code contributions, mentorship, and technical insights."}/>
+            <div className={"mx-72 mt-10 grid grid-cols-3 gap-3"}>
+                {DEVELOPERS.map((developer) => (
+                    developer.type === "top-rated" &&
+                    <DeveloperCard key={developer.id} id={developer.id}
+                                   username={developer.username}
+                                   description={developer.description} pic={developer.pic}/>
+                ))}
+            </div>
+        </>
     )
 }
 export default TopRated
